3462498740be53d1c7ae9d188423a6bda79e3344
Mario <lombardomario01@gmail.com>
2022-01-17 19:14:46 +0100

    difference between two completed text files

--- differences_between_two_files.php
+++ differences_between_two_files.php
@@ -1,3 +1,27 @@
+<?php
+
+    include "custom-error-handler.php";
+
+    $textFile1 = fopen('test1.txt','r');
+
+    $f1 = [];
+    while ($line = fgets($textFile1)) {
+        $f1[] = $line;
+    }
+
+    fclose($textFile1);
+
+    
+    $textFile2 = fopen('test2.txt','r');
+
+    $f2 = [];
+    while ($line = fgets($textFile2)) {
+        $f2[] = $line;
+    }
+
+    fclose($textFile2);
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,11 +32,6 @@
     <link rel="stylesheet" href="css/style.css">
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
-    <?php
-        include "custom-error-handler.php";
-    ?>
-
 </head>
 <body>
     
@@ -28,35 +47,30 @@
     <main>    
         <!-- Custom container -->
         <div class="ms-container">
-            <h2>Insert two files and check their differences</h2>
+            <h2>Difference between two text files</h2>
 
             <!-- Container first file -->
             <div id="container_first_file">
-                <form method="post" enctype="multipart/form-data">
-                    <div class="ms-input-group input-group">
-                        <input type="file" class="form-control" id="first_file" name="first_file" aria-label="Upload">
-                        <button class="btn btn-outline-secondary" type="submit" id="btn_first_file">View information file</button>
-                    </div>
-                </form>
-
-                <?php
-                    // Test to see if it works.
-                    if (isset($_FILES['first_file']) != null) {
-                        echo 'file name:' . ' ' . $_FILES['first_file']['name'] . '<br>';
-                        echo 'file type:' . ' ' . $_FILES['first_file']['type'] . '<br>';
-                    }
-                ?>
-
+                <div class="preview">
+                <?php foreach ($f1 as $key => $line) {
+                    echo '<div class="number">' . $key . '</div><div class="text">' . $line . '</div>';
+                } ?>
+                </div>
             </div>
-
+            
             <!-- Container second file -->
-            <div id="container_second_file">
-                <form method="post" enctype="multipart/form-data">
-                    <div class="ms-input-group input-group">
-                        <input type="file" class="form-control" id="second_file" name="second_file" aria-label="Upload">
-                        <button class="btn btn-outline-secondary" type="submit" id="btn_second_file">View information file</button>
-                    </div>
-                </form>
+            <div id="container_second_file" class="d-block">
+                <div class="preview">
+                <?php foreach ($f2 as $key => $line) {
+                    if (isset($f2[$key]) == isset($f1[$key]) && $f2[$key] === $f1[$key]) {
+                        echo '<div class="number">' . $key . '</div><div class="text">' . $line . '</div>';
+                    } else if (isset($f2[$key]) != isset($f1[$key])) {
+                        echo '<div class="number"><span class="text-success">' . $key . '</div><div class="text text-success"> ' . $line . '</div></span>';
+                    } else {
+                        echo '<div class="number"><span class="text-danger">' . $key . '</div><div class="text text-danger"> ' . $line . '</div></span>';
+                    }
+                } ?>
+                </div>
             </div>
         </div>
     </main>
